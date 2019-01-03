6c845d61e3c695bf46a361d0ee4dfa44c4cafcd0
Alex Gamper <agamper@bigpond.net.au>
2019-01-03 22:13:10 +1100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//----------------------------------------------------------------------------------------------------//
 // MIT License
 //
-// Copyright (c) 2019 Alex Gamper
+// Copyright (c) 2019 Alexander Gamper, All Rights Reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
